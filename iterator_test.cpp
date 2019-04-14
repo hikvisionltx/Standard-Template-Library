b962c795a7423698403be5b9ff1a4ed0899af262
litmxs <litmxs@gmail.com>
2019-04-14 17:22:22 +0800

    迭代器

---
+++ iterator_test.cpp
@@ -0,0 +1,125 @@
+#include "stl_iterator.h"
+#include <typeinfo>
+#include <iostream>
+using namespace std;
+
+
+template <typename T>
+class ListItem
+{
+public:
+    ListItem() {_next = nullptr;}
+    ListItem(T value, ListItem<T> * next)
+    {
+        _value = value;
+        _next = next;
+    }
+    T _value;
+    ListItem<T> * _next;
+};
+
+
+template <typename T>
+class List
+{
+public:
+    void insert_front(T value)
+    {
+        if(_front == nullptr)
+        { 
+            ListItem<T> * node = new ListItem<T>(value, nullptr);
+            _front = _end = node;
+        }
+        else 
+        {
+            ListItem<T> * node = new ListItem<T>(value, _front);
+            _front = node;
+        }
+    }
+    void inerst_end(T value)
+    {
+        if(_front == nullptr)
+        {
+            ListItem<T> *node = new ListItem<T>(value, nullptr);
+            _front = _end = node;
+        }
+        else 
+        {
+            ListItem<T> *node = new ListItem<T>(value, nullptr);
+            _end->_next = node;
+            _end = node;
+        }
+    }
+    void display(std::ostream &os = std::cout) const
+    {
+        ListItem<T> * t = _front;
+        while(t != nullptr)
+        {
+            os << t->_value << " ";
+            t = t->_next;
+        }
+    }
+
+    List()
+    {
+        _front = _end = nullptr;
+    }
+    ListItem<T> * _end;
+    ListItem<T> * _front;
+    long _size;
+};
+
+template <typename Item>
+struct ListIter :public ltx::iterator<Item, long>
+{
+    Item * ptr;
+    ListIter(Item * p = nullptr) :ptr(p){}
+    Item& operator *() const {return *ptr;}
+    Item* operator ->() const {return ptr;}
+
+    ListIter& operator ++()
+    {
+        ptr = ptr->_next;
+        return *this;
+    }
+    ListIter& operator ++(int)
+    {
+        ListIter tmp = *this;
+        ++*this;
+        return tmp;
+    }
+    bool operator ==(const ListIter& i) const 
+    {
+        return ptr==i.ptr;
+    }
+    bool operator !=(const ListIter& i) const
+    {
+        return ptr!=i.ptr;
+    }
+
+};
+
+
+int main()
+{
+    using namespace ltx;
+    List<int> list;
+    list.inerst_end(10);
+    list.inerst_end(20);
+    list.insert_front(20);
+    list.inerst_end(30);
+    list.display();
+    ListIter<ListItem<int>> begin(list._front);
+    ListIter<ListItem<int>> end;
+    ListIter<ListItem<int>> iter;
+    for(iter = begin; iter!=end; ++iter)
+    {
+        cout << iter->_value << endl;
+    }
+
+
+    
+
+
+    return 0;
+}
--- stl_iterator.h
+++ stl_iterator.h
@@ -1,11 +1,138 @@
 #ifndef STL_ITERATOR_H
 #define STL_ITERATOR_H
+#include <cstddef>
+namespace ltx
+{
+    struct input_iterator_tag {};
+    struct output_iterator_tag {};
+    struct forward_iterator_tag :public input_iterator_tag {};
+    struct bidirectional_iterator_tag :public forward_iterator_tag {};
+    struct random_access_iterator_tag :public bidirectional_iterator_tag {};
 
-struct input_iterator_tag {};
-struct output_iterator_tag {};
-struct forward_iterator_tag :public input_iterator_tag {};
-struct bidirectional_iterator_tag :public forward_iterator_tag {};
-struct random_accesss_iterator_tag :public bidirectional_iterator_tag {};
+    template <typename Category, typename T, typename Distance = ptrdiff_t, 
+    typename Pointer = T*, class Reference =  T&>
+    struct iterator
+    {
+        typedef Category    iterator_category;
+        typedef T           value_type;
+        typedef Distance    difference_type;
+        typedef Pointer     pointer;
+        typedef Reference   reference;
+    };
+    
+    // traits 
+    template <typename Iterator>
+    struct iterator_traits
+    {
+        typedef typename Iterator::iterator_category    iterator_category;
+        typedef typename Iterator::value_type           value_type;
+        typedef typename Iterator::difference_type      difference_type;
+        typedef typename Iterator::pointer              pointer;
+        typedef typename Iterator::reference            reference;
+    };
+
+    template <typename T>
+    struct iterator_traits<T*>
+    {
+        typedef random_access_iterator_tag     iterator_category;
+        typedef T                               value_type;
+        typedef ptrdiff_t                       difference_type;
+        typedef T*                              pointer;
+        typedef T&                              reference;
+    };    
+    
+    template <typename T>
+    struct iterator_traits<const T*>
+    {
+        typedef random_access_iterator_tag     iterator_category;
+        typedef T                               value_type;
+        typedef ptrdiff_t                       difference_type;
+        typedef const T*                        pointer;
+        typedef const T&                        reference;
+    };
+
+    // 
+    template <typename Iterator>
+    inline typename iterator_traits<Iterator>::iterator_category
+    iterator_category(const Iterator &)
+    {
+        typedef typename iterator_traits<Iterator>::iterator_category category;
+        return category();
+    }
+    template <typename Iterator>
+    inline typename iterator_traits<Iterator>::difference_type*
+    distance_type(const Iterator&)
+    {
+        return static_cast<typename iterator_traits<Iterator>::difference_type*>(0);
+    }
+
+    template <typename Iterator>
+    inline typename iterator_traits<Iterator>::value_type*
+    value_type(const Iterator&)
+    {
+        return static_cast<typename iterator_traits<Iterator>::value_type*>(0);
+    }
+
+
+    // 输入迭代器
+    template <typename InputIterator>
+    inline typename iterator_traits<InputIterator>::difference_type
+    __distance(InputIterator first, InputIterator last, input_iterator_tag)
+    {
+        typename iterator_traits<InputIterator>::difference_type n = 0;
+        while(first != last)
+        {
+            ++first;
+            ++n;
+        }
+        return n;
+    }
+    //随机迭代器
+    template <typename RandomAccessIterator>
+    inline typename iterator_traits<RandomAccessIterator>::difference_type
+    __distance(RandomAccessIterator first, RandomAccessIterator last, 
+    random_access_iterator_tag)
+    {
+        return last-first;
+    }
+
+    template <class InputIterator>
+    inline typename iterator_traits<InputIterator>::difference_type
+    distance(InputIterator first, InputIterator last)
+    {
+        typedef typename iterator_traits<InputIterator>::iterator_category category;
+        return __distance(first, last, category());
+    }
+
+
+    template <typename InputIterator, typename Distance>
+    inline void __advance(InputIterator &i, Distance n, input_iterator_tag)
+    {
+        while(n--) ++i;
+    }
+    template <typename BidirectionalIterator, typename Distance>
+    inline void __advance(BidirectionalIterator &i, Distance n, bidirectional_iterator_tag)
+    {
+        if(n>=0) 
+            while(n--) ++i;
+        else 
+            while(n++) --i;
+    }
+    template <typename RandomAccessIterator, typename Distance>
+    inline void __advance(RandomAccessIterator &i, Distance n, random_access_iterator_tag)
+    {
+        i += n;
+    }
+    template <typename InputIterator, typename Distance>
+    inline void advance(InputIterator &i, Distance n, input_iterator_tag)
+    {
+        __advance(i, n, iterator_category(i));
+    }
+
+
+    
+
+}
 
 
 #endif
